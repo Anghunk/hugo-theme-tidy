9e8e5908f28681c11babd54d4bf950aed40a0a61
lovezsh <shuxhan@163.com>
2023-03-07 11:22:07 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+## hugo-theme-tidy
+
+一款hugo博客。
+
+简约大气，适配手机端，灯箱等功能。
+
+![image](https://cdn.staticaly.com/gh/lovezsh/pic-cdn@main/20230307/image.37ezklv93yg.webp)
---
+++ static/js/main.js
@@ -0,0 +1,34 @@
+$(function () {
+  $('.menu-btn').click(function () {
+    $("header .sticky").slideToggle();
+    $('.opacity').toggle();
+  })
+
+  $('.opacity').click(function () {
+    $("header .sticky").slideToggle();
+    $('.opacity').toggle();
+  })
+
+  $('.content a').attr('target', '_blank');
+  $('#TableOfContents a').attr('target', '')
+  $('.content img').addClass('slb');
+  $('.slb').simplebox({ fadeSpeed: 100 });
+
+  $('iframe').wrap('<p class="iframe"></p>')
+
+  $(document).ready(function () {
+    if (window.location.pathname == "/") {
+      $(".nav a:eq(0)").addClass("active");
+    }
+    a = window.location;
+    $(".nav a").each(function () {
+      b = String($(this).attr("href")).replace("/", "");
+      if (String(a).indexOf(b) > 0) {
+        $(this).closest("a").addClass("active");
+      }
+    });
+  });
+
+  $('.atk-main-editor>.atk-bottom').after($('.atk-main-editor>.atk-plug-panel-wrap'))
+
+})
